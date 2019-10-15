aef501fcdf8d83728d0e8abf1f238c405068f0e8
cy275 <cy275@njit.edu>
2019-10-15 17:24:54 -0400

    Fix: removed a print function

--- src/my_script.py
+++ src/my_script.py
@@ -5,5 +5,3 @@ from pystrich.datamatrix import DataMatrixEncoder
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
-
-print('Let me create a branch, add a print function, then commit and merge')
